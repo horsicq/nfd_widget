94589f4cab29c79a01f775ac6aa93c84127afbc1
horsicq <horsicq@gmail.com>
2021-11-08 19:04:04 +0100

    Fix: 2021-11-08

--- nfd_widget.cmake
+++ nfd_widget.cmake
@@ -1,1 +1,11 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+include(${CMAKE_CURRENT_LIST_DIR}/../StaticScan/formresult.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogtextinfo.cmake)
+
+set(NFD_WIDGET_SOURCES
+    ${FORMRESULT_SOURCES}
+    ${DIALOGTEXTINFO_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/nfd_widget.ui
+    ${CMAKE_CURRENT_LIST_DIR}/nfd_widget.cpp
+)
