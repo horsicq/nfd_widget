834c1785da7a9f1cd995402088f2c29bf3571eea
horsicq <horsicq@gmail.com>
2023-08-07 18:58:50 +0200

    Fix: 2023-08-07

--- dialognfdscandirectory.h
+++ dialognfdscandirectory.h
@@ -51,7 +51,7 @@ private slots:
     void on_pushButtonSave_clicked();
 
 signals:
-    void resultSignal(QString sText);
+    void resultSignal(const QString &sText);
 
 private:
     Ui::DialogNFDScanDirectory *ui;
